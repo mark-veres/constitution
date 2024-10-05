583d7c11eaaeaa6781aabd979e24f8a88a728969
Mark Vereș <mark@markveres.ro>
2024-10-06 00:54:07 +0300

    add support for paragraph subpoints

--- scraper.js
+++ scraper.js
@@ -1,15 +1,24 @@
-function scrape() {
+function getParagraphs(elem) {
+    let paragraphs = [...elem.childNodes].filter(c => (c.nodeName == "#text") || (c.nodeName == "P"))
+
+    return paragraphs.map(p => {
+        if (p.childNodes.length == 0) return { "content": p.data.trim(), "subpoints": [] }
+
+        let txts = [...p.childNodes].filter(c => c.nodeName == "#text")
+        let content = txts.shift()
+        return { "content": content.data.trim(), "subpoints": txts.map(t => t?.data?.trim()) }
+    })
+}
+
+function pageToJSON() {
     let elements = [...document.querySelectorAll("tr[valign=TOP]")]
     elements.shift()
 
     let articles = elements.map(elem => {
-        let paragraphs = elem.children[1].innerText.split("\n").filter(p => p != "")
-        paragraphs.shift()
-
         return {
             "id": elem.children[1].querySelector("b").innerText.split(" ")[1],
             "name": elem.children[0].innerText,
-            "paragraphs": paragraphs
+            "paragraphs": getParagraphs(elem.children[1].childNodes[0])
         }
     })
 
@@ -20,6 +29,12 @@ function scrape() {
         "name": title[1],
         "articles": articles
     }
+}
+
+function scrape() {
+    let result = JSON.parse(localStorage.getItem("result")) ?? []
+    result.push(pageToJSON())
+    localStorage.setItem("result", JSON.stringify(result))
 }
 
 scrape()
